439ed66ccbd57b48e15abc9a0bf8382be0039da5
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2024-05-20 00:33:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+from urllib import request
 from w1thermsensor import W1ThermSensor
 
 # Initialize the sensor
@@ -7,10 +8,16 @@ sensor = W1ThermSensor()
 while True:
     # Read the temperature
     temperature_celsius = sensor.get_temperature()
-    temperature_fahrenheit = sensor.get_temperature(W1ThermSensor.DEGREES_F)
+    #temperature_fahrenheit = sensor.get_temperature(W1ThermSensor.DEGREES_F)
     
     # Print the temperature
-    print(f"Temperature: {temperature_celsius:.2f}°C / {temperature_fahrenheit:.2f}°F")
+    print(f"Temperature: {temperature_celsius:.2f}°C")
+
+
+    form_url = "https://docs.google.com/forms/d/e/1FAIpQLSck0PwBvp_kwBuYo6aXPpm7bR8SmiCUmsrreE6DkoVRF32N6w/formResponse?usp=pp_url&entry.1936681108={}&submit=Submit".format(temperature_celsius)
+  
+  
+    request.urlopen(form_url)
     
     # Wait for a second before reading again
     time.sleep(1)
